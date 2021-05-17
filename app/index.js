c217aed7ff4341c1b366cdf161cabcf4162e87e9
Harrison Howard <harrison.howard00707@gmail.com>
2021-05-17 15:01:50 +1000

    Fixed typo

--- app/index.js
+++ app/index.js
@@ -13,7 +13,7 @@ app.get("/", (req, res) => {
 });
 
 app.get("/users", db.getUsers);
-app.get("/users/:id", db.getUserById);
+app.get("/users/:id", db.getUserByID);
 app.post("/users", db.createUser);
 app.put("/users/:id", db.updateUser);
 app.delete("/users/:id", db.deleteUser);
